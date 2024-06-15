0cd932ee1a8e60ee5c0bca5103855d51bd6be2d2
Mark Smith <smith.h.mark@gmail.com>
2024-06-15 14:19:45 -0600

    created an agent component

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct Agent;
+
+#[derive(Component)]
+pub struct Name(String);
+
 fn hello_world() {
     println!("Hello, world! (from a system)");
 }
 
+fn agent_names(query: Query<&Name, With<Agent>>) {
+    for agent in &query {
+        println!("Agent name: {}", agent.0);
+    }
+}
+
+fn add_agents(mut commands: Commands) {
+    commands.spawn((Agent, Name("Smith".to_string())));
+}
+
 fn main() {
     println!("Hello, world!");
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_agents)
+        .add_systems(Update, agent_names)
+        .add_systems(Update, hello_world)
+        .run();
 }
